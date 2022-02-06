62c91b761188c9db4e4c4dcd45990b39051b82a8
xwosic <mcisow@gmail.com>
2022-02-06 20:51:39 +0100

    distribution based on rect

--- game/player/card_zone.py
+++ game/player/card_zone.py
@@ -1,4 +1,3 @@
-from typing import List
 import pygame
 from game.player.zone import Zone
 from game.card import Card, CardVisualization
@@ -12,27 +11,47 @@ class CardZone(Zone):
     def add_card(self, card):
         if isinstance(card, CardVisualization):
             self.cards.add(card)
+            # self.rotate_card_if_new(card)
             card.loc = self
             if len(self.cards) > 0:
                 self.distribute_cards()
 
         elif isinstance(card, Card):
             self.cards.add(card.view)
+            # self.rotate_card_if_new(card.view)
             card.view.loc = self
             if len(self.cards) > 0:
                 self.distribute_cards()
             
     def remove_card(self, card: CardVisualization):
         if card in self.cards:
+            # self.undo_rotate_card(card)
             self.cards.remove(card)
-            card.loc = None
         return card.name
+    
+    def rotate_card_if_new(self, view):
+        if view.loc is not self:
+            print('its new me')
+            if self.is_rotated():
+                self.rotate_card(view)
+    
+    def rotate_card(self, view):
+        position = view.rect.center
+        view.image = pygame.transform.rotate(view.image, -self.a)
+        view.rect = view.image.get_rect()
+        view.rect.center = position
+    
+    def undo_rotate_card(self, view):
+        position = view.rect.center
+        view.image = pygame.transform.rotate(view.image, self.a)
+        view.rect = view.image.get_rect()
+        view.rect.center = position
 
     def distribute_cards(self):
         # count space between cards
         cards_count = len(self.cards)
-        space_x = self.w // (cards_count + 1)
-        space_y = self.h // (cards_count + 1)
+        space_x = self.rect.width // (cards_count + 1)
+        space_y = self.rect.height // (cards_count + 1)
 
         # map coordinates with objects
         coordinate_dict = {}
@@ -50,8 +69,8 @@ class CardZone(Zone):
         # distribute
         for number, coordinate in enumerate(list_of_order):
             if self.is_rotated():
-                coordinate_dict[coordinate].rect.centerx = self.x + self.w // 2
-                coordinate_dict[coordinate].rect.centery = self.y + space_y * (number + 1)
+                coordinate_dict[coordinate].rect.centerx = self.rect.x + self.rect.width // 2
+                coordinate_dict[coordinate].rect.centery = self.rect.y + space_y * (number + 1)
             else:            
-                coordinate_dict[coordinate].rect.centerx = self.x + space_x * (number + 1)
-                coordinate_dict[coordinate].rect.centery = self.y + self.h // 2
+                coordinate_dict[coordinate].rect.centerx = self.rect.x + space_x * (number + 1)
+                coordinate_dict[coordinate].rect.centery = self.rect.y + self.rect.height // 2
