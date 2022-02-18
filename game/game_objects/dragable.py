9a7ed44854cf7aefd5183bc7100512b16b1726e0
xwosic <mcisow@gmail.com>
2022-02-18 18:08:53 +0100

    add drops to dragable

--- game/game_objects/dragable.py
+++ game/game_objects/dragable.py
@@ -10,6 +10,7 @@ class Dragable(Clickable):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.loc = None
+        self.default_drops = ['Pile', 'CardZone']
 
     def drop_into(self, pos):
         """
@@ -17,16 +18,20 @@ class Dragable(Clickable):
         All players' zones are checked and object is assigned to
         the first matching and removed from previous one.
         """
-        for player in self.game.players:
-            for pile in player.piles:
-                if pile.view.is_clicked(pos):
-                    self.loc.remove_card(self)
-                    self.loc = None
-                    pile.view.put_card_on_top(self)
 
-            for zone in player.zones:
-                if zone.is_clicked(pos):
-                    self.loc.remove_card(self)
-                    self.loc = None
-                    zone.add_card(self)
-                    break
+        for player in self.game.players:
+            if 'Pile' in self.default_drops:
+                for pile in player.piles:
+                    if pile.view.is_clicked(pos):
+                        self.loc.remove_card(self)
+                        self.loc = None
+                        pile.view.put_card_on_top(self)
+                        return
+
+            if 'CardZone' in self.default_drops:
+                for zone in player.zones:
+                    if zone.is_clicked(pos):
+                        self.loc.remove_card(self)
+                        self.loc = None
+                        zone.add_card(self)
+                        return
